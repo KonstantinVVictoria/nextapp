9605ba899df446f0d1e4c40648efc48191058128
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-10-11 05:30:48 -0700

    First commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // mathching all API routes
-        source: "/api/*",
+        source: "./pages/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
