9230b66c57f0e63b25ce19c89321a43ef4ee48aa
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-10-11 06:33:15 -0700

    First commit

--- components/console.js
+++ components/console.js
@@ -6,7 +6,7 @@ let uniqueID = Math.floor(
   Math.random(Date.UTC()) * Math.floor(Math.pow(10, 12))
 );
 let peer = new Peer(uniqueID, {
-  host: "https://peerjsserver.vercel.app/",
+  host: "https://peerjsserver.vercel.app",
   port: 9000,
   path: "/myapp",
 });
