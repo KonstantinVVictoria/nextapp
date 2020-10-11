b196698d4c82503986122f2e555fa635351b8bd7
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-10-11 05:40:54 -0700

    First commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // mathching all API routes
-        source: "./pages/api/:path*",
+        source: "/api/signin",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
@@ -16,6 +16,21 @@ module.exports = {
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
+          {
+            // mathching all API routes
+            source: "/api/queryUser",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value:
+                  "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              },
         ],
       },
     ];
