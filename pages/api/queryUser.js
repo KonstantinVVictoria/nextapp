eb7011b7597820da39cc980470000a15a9eb8ac0
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-10-11 06:05:39 -0700

    First commit

--- pages/api/queryUser.js
+++ pages/api/queryUser.js
@@ -1,6 +1,23 @@
 import data from "./data";
 let { people } = data;
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
 
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 const isIDTaken = (id) => {
   if (people[`${id}`]) return people[`${id}`].name;
   else {
@@ -9,6 +26,7 @@ const isIDTaken = (id) => {
 };
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   let name = null;
   console.log(req.body, 1);
   if (req.method === "POST") {
--- pages/api/signin.js
+++ pages/api/signin.js
@@ -1,8 +1,26 @@
 import data from "./data";
 let { people } = data;
 
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 const isIDTaken = (person) => {
   console.log(people[`${person.id}`]);
@@ -14,6 +32,7 @@ const isIDTaken = (person) => {
 };
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   let isTaken = null;
   console.log(req.body, 1);
   if (req.method === "POST") {
